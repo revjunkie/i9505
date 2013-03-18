98e44c3aefcc37f0bd1f52301ef7abe75f068325
Keun-O Park <kpark3469@gmail.com>
2013-03-18 07:32:53 +0100

    ARM: 7676/1: fix a wrong value returned from CALLER_ADDRn
    This makes return_address() return a correct value for CALLER_ADDRn.
    To have a correct value from CALLER_ADDRn, we need to fix three points.
    
    * The unwind_frame() does not update frame->lr but frame->pc for backtrace.
    So frame->pc is meaningful for backtrace.
    
    * data.level should be adjusted by adding 2 additional iteration levels.
    With the current +1 level adjustment, the result of CALLER_ADDR1 will
    be the same return address with CALLER_ADDR0.
    
    * The initialization of data.addr to NULL is needed.
    When unwind_fame() fails right after data.level reaches zero,
    the routine returns data.addr which has uninitialized garbage value.
    
    Signed-off-by: Sahara <keun-o.park@windriver.com>
    Reviewed-by: Dave Martin <dave.martin@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -26,7 +26,7 @@ static int save_return_addr(struct stackframe *frame, void *d)
 	struct return_address_data *data = d;
 
 	if (!data->level) {
-		data->addr = (void *)frame->lr;
+		data->addr = (void *)frame->pc;
 
 		return 1;
 	} else {
@@ -40,7 +40,8 @@ void *return_address(unsigned int level)
 	struct return_address_data data;
 	struct stackframe frame;
 
-	data.level = level + 1;
+	data.level = level + 2;
+	data.addr = NULL;
 
 	frame.fp = (unsigned long)__builtin_frame_address(0);
 	frame.sp = current_stack_pointer;
