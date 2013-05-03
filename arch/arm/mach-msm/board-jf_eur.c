aa4118eebda5c500e8fcdbe052f62dd99c333cb5
Praveen Chidambaram <pchidamb@codeaurora.org>
2013-05-03 12:52:42 -0600

    msm: thermal: Add cpu mask control for frequency limiting
    Limit the number of cpus affected by kernel thermal module's frequency
    control mechanism by specifying the participating cpus in the
    freq-control-mask. Frequency control can be disabled, if the mask is 0.
    
    Change-Id: Iab659730dd06ce59521917c8ba6210d7dd1311b0
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>

--- arch/arm/mach-msm/board-jf_eur.c
+++ arch/arm/mach-msm/board-jf_eur.c
@@ -3232,11 +3232,12 @@ static struct platform_device msm_tsens_device = {
 };
 
 static struct msm_thermal_data msm_thermal_pdata = {
-	.sensor_id = 7,
+	.sensor_id = 0,
 	.poll_ms = 250,
 	.limit_temp_degC = 70,
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
+	.freq_control_mask = 0xf,
 	.core_limit_temp_degC = 80,
 	.core_temp_hysteresis_degC = 10,
 	.core_control_mask = 0xe,
